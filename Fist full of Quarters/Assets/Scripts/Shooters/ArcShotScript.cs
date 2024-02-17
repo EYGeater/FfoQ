912c0043108258ad66eb8a73b6c272299bd6c34e
CADEN <ca747322@ucf.edu>
2024-02-17 14:46:18 -0500

    arc shot wip

--- Fist full of Quarters/Assets/Scripts/Shooters/ArcShotScript.cs
+++ Fist full of Quarters/Assets/Scripts/Shooters/ArcShotScript.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StraightShotScript : ShootScript
+public class ArcShotScript : ShootScript
 {
     protected override void Start()
     {
-        playerController.yellowEvent += Shoot;
+        playerController.greenEvent += Shoot;
     }
 }
--- Fist full of Quarters/Assets/Scripts/Shooters/ShootScript.cs
+++ Fist full of Quarters/Assets/Scripts/Shooters/ShootScript.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public abstract class ShootScript : MonoBehaviour
 {
+    public PlayerController playerController;
+    public ObjectPool pool;
     public ShotData shotData;
     public Transform shootPoint;
 
     protected bool canShoot = true;
-    public abstract void Shoot();
     public void StartCooldown()
     {
         StartCoroutine(CooldownRoutine());
     }
+
+    protected abstract void Start();
     protected IEnumerator CooldownRoutine()
     {
         canShoot = false;
         yield return new WaitForSeconds(shotData.cooldown);
         canShoot = true;
+    }
+
+    public void Shoot()
+    {
+        if (!canShoot) return;
+        GameObject shot = pool.PullFromPool();
+        if (shot != null)
+        {
+            shot.SetActive(true);
+            shot.transform.position = shootPoint.position;
+            shot.GetComponent<Shot>().Fire(playerController.transform.forward, shotData);
+
+            StartCooldown();
+        }
     }
 }
